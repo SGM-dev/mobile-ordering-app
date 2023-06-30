cb8f922c5861e51ca87eddc7d769f2118ac37092
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-06-30 23:18:04 +0530

    Rate your Experience

--- index.js
+++ index.js
@@ -167,5 +167,27 @@ function renderStatus() {
   orderCompleteSection.innerHTML = `          
   <div class="order-status">
     <p class="order-text">Thanks, ${customerName}! Your order is on its way!</p>
+    <form class="rating-form" id="rating-form">
+    <label class="rating-label" for="rating">
+    Rate Your Experience</label>
+    <div class="rating-stars">
+      <input
+      class="rating rating-star"
+      id="rating"
+      max="5"
+      min="1"
+      step="1"
+      type="range"
+      value="3"
+      list="values">
+      <datalist id="values">
+
+      <option value="1" label="1"></option>
+      <option value="2" label="2"></option>
+      <option value="3" label="3"></option>
+      <option value="4" label="4"></option>
+      <option value="5" label="5"></option>
+      </datalist>
+  </form>
   </div>`;
 }
